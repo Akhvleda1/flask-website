788914e19e4a413d3cad17309753b1e9d7274a72
Akhvledaa <luka.akhvlediani.1@btu.edu.ge>
2023-06-18 13:16:17 +0400

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, redirect, url_for, request, session
+
+app = Flask(__name__)
+app.secret_key = 'secretkey'
+
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+# @app.route('/')
+# def index():
+#     return render_template('Base.html')
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about')
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == "POST":
+        user_email = request.form['email']
+        session['user'] = user_email
+        return redirect(url_for('user', email=user_email))
+    else:
+        return render_template('login.html', title='Log in')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('home'))
+
+
+@app.route('/user/<email>')
+def user(email):
+    if "user" in session:
+        return render_template('user.html', email=email)
+    else:
+        return redirect(url_for('login'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
