7fec3943732bb91efd4111f302f12c65df35ef46
Akhvledaa <luka.akhvlediani.1@btu.edu.ge>
2023-06-21 22:45:55 +0400

    added user database

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whiskey_db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_db.sqlite'
 
 db = SQLAlchemy(app)
 
@@ -13,69 +13,63 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String)
     email = db.Column(db.String)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String)
 
 
+with app.app_context():
+    db.create_all()
+    # user1 = User(username='levani', email='levani@gmail.com', password='password')
+    # db.session.add(user1)
+    # db.session.commit()
+    # allnames = User.query.all()
+    # for each in allnames:
+    #     print(each.username)
+
 cards = [
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
         'title': 'Card 1',
-        'description': 'This is the card.',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 2',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 3',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 4',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 5',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 6',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 7',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
     {
-        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
-        'title': 'Card 7',
-        'description': 'This is the card.',
+        'image': 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+        'title': 'Card 1',
+        'price': '$40.00 - $80.00',
     },
-
     # Add more card objects as needed
 ]
 
 
-# def extract_name_from_email(email):
-#     username, domain = email.split('@')
-#
-#     # Remove any dots from the username
-#     username = username.replace('.', ' ')
-#
-#     # Split the username into parts
-#     name_parts = username.split()
-#
-#     # Capitalize the first letter of each part and join them
-#     name = ' '.join(part.capitalize() for part in name_parts)
-#
-#     return name
 
 
 @app.route('/')
@@ -100,17 +94,36 @@ def login():
         user_email = request.form['email']
         password = request.form['password']
         if user_email == '' or password == '':
-            flash('make sure to fill in all fields')
+            flash('Please fill in all the required fields.')
             return redirect(url_for('login'))
-        session['user'] = user_email
-        return redirect(url_for('home'))
+        user = User.query.filter_by(email=user_email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                session['user'] = user_email
+                return redirect(url_for('home'))
+            else:
+                return 'paroli arasworia'
+        else:
+            return 'user ar aris'
+
+        # return redirect(url_for('home'))
     else:
         return render_template('login.html', title='Log in')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    return render_template('signup.html', title='Sign up')
+    if request.method == "POST":
+        username = request.form['username']
+        user_email = request.form['email']
+        password = request.form['password']
+        if username == '' or user_email == '' or password == '':
+            flash('Please fill in all the required fields.')
+            return redirect(url_for('signup'))
+        session['user'] = user_email
+        return redirect(url_for('home'))
+    else:
+        return render_template('signup.html', title='Sign up')
 
 
 @app.route('/logout')
