3156e17c2f0466f160933d698c94e09dcaa174ce
Akhvledaa <luka.akhvlediani.1@btu.edu.ge>
2023-06-21 20:28:32 +0400

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///whiskey_db.sqlite'
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String)
+
 
 cards = [
     {
@@ -39,8 +53,29 @@ cards = [
         'title': 'Card 7',
         'description': 'This is the card.',
     },
+    {
+        'image': 'https://cdn.discordapp.com/attachments/1119903896115097668/1120416421692850246/generate_logo_for_Whiskey_with_t.png',
+        'title': 'Card 7',
+        'description': 'This is the card.',
+    },
+
     # Add more card objects as needed
 ]
+
+
+# def extract_name_from_email(email):
+#     username, domain = email.split('@')
+#
+#     # Remove any dots from the username
+#     username = username.replace('.', ' ')
+#
+#     # Split the username into parts
+#     name_parts = username.split()
+#
+#     # Capitalize the first letter of each part and join them
+#     name = ' '.join(part.capitalize() for part in name_parts)
+#
+#     return name
 
 
 @app.route('/')
@@ -63,16 +98,18 @@ def shop():
 def login():
     if request.method == "POST":
         user_email = request.form['email']
+        password = request.form['password']
+        if user_email == '' or password == '':
+            flash('make sure to fill in all fields')
+            return redirect(url_for('login'))
         session['user'] = user_email
-        return redirect(url_for('user', email=user_email))
+        return redirect(url_for('home'))
     else:
         return render_template('login.html', title='Log in')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    # if request.form = 'POST':
-
     return render_template('signup.html', title='Sign up')
 
 
@@ -82,18 +119,13 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/user/<email>]')
-def user(email):
-    if "user" in session:
-        return render_template('user.html', email=email)
-    else:
-        return redirect(url_for('login'))
-
-
 @app.route('/profile')
 def profile():
-    user_email = request.args.get('email')
-    return render_template('profile.html', email=user_email)
+    user_email = session.get('user')
+    if 'user' in session:
+        return render_template('profile.html', email=user_email)
+    else:
+        return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
