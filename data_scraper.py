d58ce1c65100e8e0e0da0c3e4be68cf50c866933
Akhvleda1 <luka.akhvlediani2504@gmail.com>
2024-05-29 20:18:28 +0400

    Fixed data_scraper.py

--- data_scraper.py
+++ data_scraper.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 import sqlite3
 import os
 
-
 url = 'https://www.thewhiskyexchange.com/c/32/irish-whiskey?'
 payloads = {
     'pg': 1
@@ -12,13 +11,8 @@ payloads = {
 current_dir = os.path.abspath(os.path.dirname(__file__))
 database_file = os.path.join(current_dir, 'instance', 'whiskeys.sqlite')
 
-con = sqlite3.connect(database_file, check_same_thread=False)
-cur = con.cursor()
 
-
-def whiskey_data():
-    whiskeys_tracking = []
-    whiskeys = []
+def parse_whiskey_data():
     # while payloads['pg'] < 2:
     response = requests.get(url, payloads)
     content = response.text
@@ -26,32 +20,43 @@ def whiskey_data():
     grid = soup.find('div', class_='product-grid')
     items = grid.find_all('li', class_='product-grid__item')
 
-    for each in items:
-        name = each.p.text.strip()
-        alc_percentage = each.find('p', class_="product-card__meta").text.strip()
-        price = each.find('p', class_="product-card__price").text.strip().replace('£', 'GBP ')
-        image_link = each.img.attrs['src']
+    with sqlite3.connect(database_file, check_same_thread=False) as con:
+        cur = con.cursor()
+        for each in items:
+            name = each.p.text.strip()
+            alc_percentage = each.find('p', class_="product-card__meta").text.strip()
+            price = each.find('p', class_="product-card__price").text.strip().replace('£', 'GBP ')
+            image_link = each.img.attrs['src']
 
-        # avoiding duplicate entries
-        if name not in whiskeys_tracking:
             # inserting data into whiskeys.sqlite file
-            cur.execute('insert into whiskeys (name, alc_percentage, price, image_link) values (?, ?, ?, ?)', [name, alc_percentage, price, image_link])
-            whiskeys_tracking.append(name)
+            cur.execute('insert into whiskeys (name, alc_percentage, price, image_link) values (?, ?, ?, ?)',
+                        [name, alc_percentage, price, image_link])
 
-            id = 1
 
-            # creating list of dictionaries to return from a function
-            whiskey = {
-                'id': id,
-                'name': name,
-                'alc_percentage': alc_percentage,
-                'price': price,
-                'image_link': image_link
-            }
-            whiskeys.append(whiskey)
-            id += 1
+def whiskey_data():
+    whiskeys_tracking = []
+    whiskeys = []
+    with sqlite3.connect(database_file, check_same_thread=False) as con:
+        cur = con.cursor()
+        data = cur.execute("select * from whiskeys").fetchall()
+        for each in data:
+            id = each[0]
+            name = each[1]
+            alc_per = each[2]
+            price = each[3]
+            image_link = each[4]
 
-    con.commit()
-    con.close()
+            # avoiding duplicate entries
+            if name not in whiskeys_tracking:
+                whiskeys_tracking.append(name)
+
+                whiskey = {
+                    'id': id,
+                    'name': name,
+                    'alc_percentage': alc_per,
+                    'price': price,
+                    'image_link': image_link
+                }
+                whiskeys.append(whiskey)
+
     return whiskeys
-
