22590fe76911702914c16b824a386883cec5c920
Akhvleda1 <luka.akhvlediani2504@gmail.com>
2024-06-05 22:19:15 +0400

    fix error

--- app/blueprints/auth/auth.py
+++ app/blueprints/auth/auth.py
@@ -35,21 +35,32 @@ def signup():
     if request.method == "POST":
         username = request.form['username']
         user_email = request.form['email']
-        phone = request.form['phone']
+        user_phone = request.form['phone']
         password = request.form['password']
         hashed_password = generate_password_hash(password)
         check_box = request.form.get('checkbox')
-        if username == '' or user_email == '' or phone == '' or password == '':
+        users = User.query.all()
+
+        # I'm using linear search since I'm going to add indexes on these columns
+        for user in users:
+            if user_email == user.email:
+                flash('The email address is already registered')
+                return redirect(url_for('auth.signup'))
+            elif user_phone == user.phone:
+                flash('The mobile number is already registered')
+                return redirect(url_for('auth.signup'))
+
+        if username == '' or user_email == '' or user_phone == '' or password == '':
             flash('Please fill in all the required fields.')
             return redirect(url_for('auth.signup'))
-        elif not phone.isdecimal():
+        elif not user_phone.isdecimal():
             flash('Phone must be a number')
             return redirect(url_for('auth.signup'))
         elif check_box is None:
             flash('You must agree to the Terms of Service')
             return redirect(url_for('auth.signup'))
         else:
-            new_user = User(username=username, email=user_email, phone=phone, password=hashed_password)
+            new_user = User(username=username, email=user_email, phone=user_phone, password=hashed_password)
             db.session.add(new_user)
             db.session.commit()
             session['user'] = user_email
